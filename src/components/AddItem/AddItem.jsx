8820e140d52b6d5ad7dea25f9cb39ae40a3a7032
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-12-20 16:32:43 -0600

    added material-Ui to additem component

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+
+
+
 
 class AddItem extends Component {
     state = {
@@ -7,26 +14,40 @@ class AddItem extends Component {
         url: ''
     }
 
-    handleChange = (name) => ({target:{value}}) => {
+    handleChange = (name) => ({ target: { value } }) => {
         this.setState({
             [name]: value
         })
     }
     handleSubmit = () => {
-        this.props.dispatch({type: 'ADD_ITEM', payload: this.state})
+        this.props.dispatch({ type: 'ADD_ITEM', payload: this.state })
         this.setState({
             description: '',
             url: ''
         })
     }
-    
+
     render() {
         return (
-            <div>
-                <input type="text" onChange={this.handleChange('description')} value={this.state.description} placeholder='Description' />
-                <input type="text" onChange={this.handleChange('url')} value={this.state.url} placeholder='Image URL' />
-                <button onClick={this.handleSubmit}>Add Item</button>
-            </div>
+            <form >
+                <TextField
+                    id="outlined-name"
+                    label="description"
+                    value={this.state.description}
+                    onChange={this.handleChange('description')}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-name"
+                    label="url"
+                    value={this.state.url}
+                    onChange={this.handleChange('url')}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <Button variant="contained" color="primary" type='submit' onClick={this.handleSubmit}>Add Item</Button>
+            </form>
         )
     }
 }
