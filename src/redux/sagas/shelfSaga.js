a7e1180bbf7eab93e75f4f308d3c5e09699f331f
Morgan Costigan <morgan.costigan@gmail.com>
2018-12-20 13:30:36 -0600

    adds shelfSaga

---
+++ src/redux/sagas/shelfSaga.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// worker Saga: will be fired on "FETCH_SHELF" actions
+function* fetchShelf() {
+    try {
+        const config = {
+            headers: { 'Content-Type': 'application/json' }, // ensures that app is looking for JSON
+            withCredentials: true,
+        };
+
+        // the config includes credentials which
+        // allow the server session to recognize the user
+        // If a user is logged in, this will return shelf information
+        // from the server session (req.user)
+        const response = yield axios.get('api/shelf', config);
+
+        // the session has given us a shelf object
+        // with an id, url, and description set the client-side shelf object
+        yield put({ type: 'SET_SHELF', payload: response.data });
+    } catch (error) {
+        console.log('Shelf get request failed', error);
+    }
+}
+
+function* shelfSaga() {
+    yield takeLatest('FETCH_SHELF', fetchShelf);
+}
+
+export default shelfSaga;
