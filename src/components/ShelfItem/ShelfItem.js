719b8a5d80e0b1ce790af9d521be671d6a1b0df8
Morgan Costigan <morgan.costigan@gmail.com>
2018-12-20 16:55:33 -0600

    shelfItem now a MUI card

--- src/components/ShelfItem/ShelfItem.js
+++ src/components/ShelfItem/ShelfItem.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+const styles = {
+    card: {
+        width: 345,
+    },
+    media: {
+        height: 150,
+    },
+};
 
 class ShelfItem extends Component {
     render() {
-        return(
-            <tr></tr>
-        )
+        const { classes } = this.props;
+
+        return (
+            <div className="shelfItem">
+                <Card className={classes.card}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image={this.props.image}
+                        />
+                        <CardContent>
+
+                            <Typography variant="h5" component="p">
+                                {this.props.description}
+                            </Typography>
+                            <Typography component="p">
+                                {this.props.person}
+                            </Typography>
+                        </CardContent>
+                        <Button size="small" color="primary">
+                            Delete
+                        </Button>
+                    </CardActionArea>
+                </Card>
+            </div>
+
+        );
+
     }
 
 }
-
-
-
-
-
-
-
 
 // Instead of taking everything from state, we just want the shelf info.
 const mapStateToProps = reduxStore => ({
@@ -25,4 +59,25 @@ const mapStateToProps = reduxStore => ({
 });
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(ShelfItem);    
+export default connect(mapStateToProps)(withStyles(styles)(ShelfItem));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// function ShelfItem(props) {
+//     const { classes } = props;
+//     return (<div></div>)
+
+// }
+
+// // ShelfItem.propTypes = {
+// //     classes: PropTypes.object.isRequired,
+// // };
+
+
+
+
+
--- src/components/ShelfList/ShelfList.js
+++ src/components/ShelfList/ShelfList.js
@@ -1,41 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import ShelfItem from '../ShelfItem/ShelfItem';
 
-class ShelfList extends Component{
+class ShelfList extends Component {
 
     componentDidMount() {
         this.getShelf();
     }
 
-    getShelf( ) {
+    getShelf() {
         console.log('getShelf');
-        
-        this.props.dispatch({ type: 'FETCH_SHELF'})
+
+        this.props.dispatch({ type: 'FETCH_SHELF' })
     }//end getShelf
 
-    render(){
-        return(
-            // <div>
-            //     <p>SHELF LIST</p>
-            //     {JSON.stringify(this.props.reduxStore.shelf)}
-            // </div>
-            <table className="ShelfList">
-                <thead>
-                    <tr>
-                        <th className="shelfImage">Image</th>
-                        <th>Description</th>
-                        <th>Person</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* Render each item from the shelf reducer */}
-                    {this.props.reduxStore.shelf.map((each) => {
-                        return (<ShelfItem key={each.id} image= {each.image_url} description={each.description} person={each.person_id} />);
-                    })}
-                </tbody>
-            </table>
+    render() {
+        return (
+            <div>
+                {/* Render each item from the shelf reducer */}
+                {this.props.reduxStore.shelf.map((each) => {
+                    return (<ShelfItem key={each.id} image={each.image_url} description={each.description} person={each.person_id} />);
+                })}
+            </div>
         )
     }
 }
