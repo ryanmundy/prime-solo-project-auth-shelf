a909b9425833f6300fbd1cd440db571aa22ba726
Travis Lang <tlang50@icloud.com>
2018-12-20 13:54:47 -0600

    add server add item route

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const rejectUnauthenticated = require('../modules/authentication-middleware');
 
 /**
  * Get all of the items on the shelf
@@ -13,8 +14,16 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
-    
+router.post('/', rejectUnauthenticated, (req, res) => {
+    const sqlText = `INSERT INTO "item"("description", "image_url")
+    VALUES($1, $2);`
+    pool.query(sqlText, [req.body.description, req.body.url])
+    .then( result => {
+        res.sendStatus(201);
+    }).catch( err => {
+        console.log('error in shelf POST:', err);
+        res.sendStatus(500);
+    })
 });
 
 
